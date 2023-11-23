624b1801bfa62c76fe29032bf062c14318e2295f
isaacnicolas <72254818+isaacnicolas@users.noreply.github.com>
2023-11-23 19:31:43 +0100

    Add random forest regressor

--- scratchml/supervised_learning/random_forest.py
+++ scratchml/supervised_learning/random_forest.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy
-from scratchml.supervised_learning.tree import Node, DecisionTreeC
+from scratchml.supervised_learning.tree import DecisionTreeC, DecisionTreeR
 
 class RandomForestC():
     """
@@ -9,6 +9,7 @@ class RandomForestC():
     Attributes:
         n_estimators (int): The number of trees in the forest.
         criterion (str): The function to measure the quality of a split.
+        splitter (str): The strategy used to split a node.
         max_depth (int): The maximum depth of the tree.
         min_samples_split (int): The minimum number of samples required to split an internal node.
         min_samples_leaf (int): The minimum number of samples required to be at a leaf node.
@@ -49,3 +50,52 @@ class RandomForestC():
         tree_preds_mode = scipy.stats.mode(tree_preds, axis=0)[0]
         
         return tree_preds_mode.flatten()
+    
+class RandomForestR():
+    """
+    A class implementing a Random Forest Regressor.
+
+    Attributes:
+        n_estimators (int): The number of trees in the forest.
+        criterion (str): The function to measure the quality of a split.
+        splitter(str): The strategy used to split a node.
+        max_depth (int): The maximum depth of the tree.
+        min_samples_split (int): The minimum number of samples required to split an internal node.
+        min_samples_leaf (int): The minimum number of samples required to be at a leaf node.
+        max_features: The number of features to consider when looking for the best split.
+    """
+    def __init__(self, n_estimators: int = None, criterion: str = 'mse', splitter: str = 'best',
+                 max_depth: int = None, min_samples_split: int = 2, min_samples_leaf: int = 1,
+                 max_features: int = None):
+        self.n_estimators = n_estimators
+        self.criterion = criterion
+        self.splitter = splitter
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.max_features = max_features
+        self.trees = [] 
+    
+    def fit(self,X,y):
+        self.trees = [DecisionTreeR(criterion = self.criterion,
+                                    splitter = self.splitter,
+                                    max_depth = self.max_depth,
+                                    min_samples_split = self.min_samples_split,
+                                    min_samples_leaf = self.min_samples_leaf,
+                                    max_features = self.max_features)
+                    for _ in range(self.n_estimators)]
+        
+        for tree in self.trees:
+            # Bootstraping: sample data with replacement
+            bootstrap_indices = np.random.randint(0,len(X),len(X))
+            X_bootstrap = X[bootstrap_indices]
+            y_bootstrap = y[bootstrap_indices]
+
+            tree.fit(X_bootstrap,y_bootstrap)
+    
+    def predict(self,X):
+        tree_preds = np.array([tree.predict(X) for tree in self.trees])
+        # Aggregate predictions: Take the mode of the predictions
+        tree_preds_mean = np.mean(tree_preds, axis=0)
+        
+        return tree_preds_mean
--- scratchml/supervised_learning/tree.py
+++ scratchml/supervised_learning/tree.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-class Node():
+class NodeC():
   """
-  A class representing a node of a decision tree model.
+  A class representing a node of a decision tree classifier model.
   """
   def __init__(self, gini = 0, entropy = 0, num_samples = 0, threshold = None,
                num_samples_per_class = None, predicted_class = None,
@@ -17,10 +17,24 @@ class Node():
     self.left = left
     self.right = right
 
+class NodeR():
+  """
+  A class representing a node of a decision tree regressor model.
+  """
+  def __init__(self, error = 0, num_samples = 0, threshold = None,
+               predicted_value = None, feature_index = None,left = None, right = None):
+    self.error = error
+    self.num_samples = num_samples
+    self.predicted_value = predicted_value
+    self.feature_index = feature_index 
+    self.threshold = threshold
+    self.left = left
+    self.right = right
+
 
 class DecisionTreeC():
   """
-  A class representing a decision tree model.
+  A class representing a decision tree classifier model.
   """
 
   def __init__(self, criterion = 'gini', splitter = 'best', max_depth = None,
@@ -44,7 +58,7 @@ class DecisionTreeC():
     predicted_class = np.argmax(num_samples_per_class)
     
     # Base node
-    node = Node (
+    node = NodeC (
       gini = self._gini(y),
       entropy = self._entropy(y),
       num_samples = len(y),
@@ -164,8 +178,155 @@ class DecisionTreeC():
     else:
       return self._predict_sample(sample,node.right)
 
+class DecisionTreeR():
+  """
+  A class representing a decision tree regressor model.
+  """
 
+  def __init__(self, criterion = 'mse', splitter = 'best', max_depth = None,
+               min_samples_split = 2, min_samples_leaf = 1, max_features = None):
+    self.criterion = criterion
+    self.splitter = splitter
+    self.max_depth = max_depth
+    self.min_samples_split = min_samples_split
+    self.min_samples_leaf = min_samples_leaf
+    self.max_features = max_features
+    self.tree_ = None
 
+  def fit(self,X,y):
+    self.classes_ = list(set(y))
+    self.n_classes_ = len(set(y))
+    self.n_features_ = X.shape[1]
+    self.tree_ = self._grow_tree(X,y)
+  
+  def _grow_tree(self,X,y,depth=0): 
+    # Base node
+    node = NodeR (
+      error = self._calculate_error(y),
+      num_samples = len(y),
+      predicted_value = np.average(y)
+    )
+
+    if depth == self.max_depth: # Check max_depth
+      return node
+    if node.num_samples < self.min_samples_split: # Check min_samples_split
+      return node
+        
+    # If the previous criteria is not met we search for the best split
+    best_feature, best_threshold = self._best_split(X,y)
+
+    if best_feature is None:
+      return node
+    
+    # Create child nodes
+    left_indices = X[:,best_feature] < best_threshold
+    X_left, y_left = X[left_indices], y[left_indices]
+    X_right, y_right = X[~left_indices], y[~left_indices]
+
+    # Check if either side is too small. If so, reutrn a leaf node and don't split
+    if len(y_left) < self.min_samples_leaf or len(y_right) < self.min_samples_leaf: 
+      return node
+
+    # Recursively create new nodes
+    node.feature_index = best_feature
+    node.threshold = best_threshold
+    node.left = self._grow_tree(X_left,y_left,depth=depth+1)
+    node.right = self._grow_tree(X_right,y_right,depth=depth+1)
+
+    return node
+
+  def _best_split(self,X,y):
+    if self.splitter == 'best':
+      # If self.max_features is None, consider all features. Otherwise randomly pick the corresponding n_features_to_pick
+      features_to_consider = range(self.n_features_)
+      if self.max_features is not None: #Consider all features
+        if self.max_features == 'sqrt':
+          n_features_to_pick = int(np.sqrt(self.n_features_))
+        elif self.max_features == 'log2':
+          n_features_to_pick = int(np.log2(self.n_features_))
+        else: # Assuming it is an integer
+          n_features_to_pick = self.max_features
+        
+        features_to_consider = np.random.choice(features_to_consider,n_features_to_pick)
+      
+      best_error = float('inf')
+      best_feature_idx = None
+      best_threshold = None
+      
+      for idx in features_to_consider:
+        possible_thresholds = set(X[:,idx])
+        for threshold in possible_thresholds:
+          # Split data based on current feature and threshold
+          left_mask = X[:,idx] < threshold
+          y_left, y_right = y[left_mask], y[~left_mask]
+
+          # Calculate the weighted impurity for the split
+          left_error = self._calculate_error(y_left)
+          right_error = self._calculate_error(y_right)
+          weighted_error = (len(y_left)/len(y))*left_error + (len(y_right)/len(y))*right_error
+
+          # Update values if the weighted entropy is better than best impurity
+          if weighted_error < best_error:
+            best_error = weighted_error
+            best_feature_idx = idx
+            best_threshold = threshold
+      
+      return best_feature_idx, best_threshold
+    
+    elif self.splitter == 'random':
+      # Randomly select a feature and a threshold
+      feature_idx = np.random.randint(self.n_features_)
+      threshold = np.random.choice(set(X[:,feature_idx]))
+
+      return feature_idx, threshold
+    
+    else:
+      raise ValueError("Please select a valid splitter option: 'best' or 'random'")
+    
+  def _calculate_error(self,y):
+    if self.criterion == 'mse':
+      return self._mse(y)
+    elif self.criterion == 'friedman_mse':
+      return self._friedman_mse(y)
+    elif self.criterion == 'mae':
+      return self._mae()
+    else:
+      raise ValueError(f"Unknown criterion {self.criterion}")
+  
+  def _mse(self,y):
+    pred_value = np.mean(y)
+    mse = np.mean((y - pred_value)**2)
+    return mse
+  
+  def _friedman_mse(self,y):
+    pred_value = np.mean(y)
+    total_variance = np.sum((y - pred_value) ** 2)
+
+    unique_values, counts = np.unique(y, return_counts=True)
+
+    weighted_variance = 0
+    for value, count in zip(unique_values,counts):
+      weighted_variance += (count / len(y)) * (value - pred_value) ** 2
+    
+    friedman_mse = (total_variance - weighted_variance) / len(y)
+
+    return friedman_mse
+  
+  def _mae(self,y):
+    median_value = np.median(y)
+    mae = np.mean(np.abs(y - median_value))
+    return mae
+  
+  def predict(self,X):
+    return np.array([self._predict_sample(sample,self.tree_) for sample in X])
+  
+  def _predict_sample(self,sample,node):
+    if node.left is None and node.right is None: #Leaf node
+      return node.predicted_value
+    if sample[node.feature_index] < node.threshold:
+      return self._predict_sample(sample,node.left)
+    else:
+      return self._predict_sample(sample,node.right)
 
 
         
